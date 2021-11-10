c0c4745e6d19471fdfce09c1c7f1ce261ccd6c6c
Gus Taylor <gustaylor2030@gmail.com>
2021-11-10 14:35:05 +0000

    Finished version

--- javascript.js
+++ javascript.js
@@ -1,8 +1,46 @@
 // Random Astrology Generator
-//lists of Arrays 
-const signs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
-const feeling = ["lucky", "unlucky","dead", "happy", "sad", "miserable", "fortunate", "missing"]
-const happening = ["avoid trees", "trust noone", "go underground", "leave your room", "go to work", "have a takeaway", "fly abroad"];
-function sentanceConstructor() {
-    const rand1 = Math.floor(Math.random() * (signs.length - 1));
+//lists of Arrays
+const signs = [
+  "Aries",
+  "Taurus",
+  "Gemini",
+  "Cancer",
+  "Leo",
+  "Virgo",
+  "Libra",
+  "Scorpio",
+  "Sagittarius",
+  "Capricorn",
+  "Aquarius",
+  "Pisces",
+];
+const feeling = [
+  "lucky",
+  "unlucky",
+  "dead",
+  "happy",
+  "sad",
+  "miserable",
+  "fortunate",
+  "missing",
+];
+const happening = [
+  "avoid trees",
+  "trust noone",
+  "go underground",
+  "leave your room",
+  "go to work",
+  "have a takeaway",
+  "fly abroad",
+];
+
+function randomArr(array) {
+  return Math.floor(Math.random() * (array.length - 1));
 }
+function sentanceConstructor() {
+  const rand1 = randomArr(signs);
+  const rand2 = randomArr(feeling);
+  const rand3 = randomArr(happening);
+  return `your sign is: ${signs[rand1]} \n you are ${feeling[rand2]} \n you should ${happening[rand3]} today.`;
+}
+console.log(sentanceConstructor());
