0422e9868ec4fe66216e6f597c00d078c5409e87
Gus Taylor <gustaylor2030@gmail.com>
2021-11-10 14:15:24 +0000

    added expression arrays

--- javascript.js
+++ javascript.js
@@ -1,2 +1,8 @@
 // Random Astrology Generator
-console.log("Hello World");
+//lists of Arrays 
+const signs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+const feeling = ["lucky", "unlucky","dead", "happy", "sad", "miserable", "fortunate", "missing"]
+const happening = ["avoid trees", "trust noone", "go underground", "leave your room", "go to work", "have a takeaway", "fly abroad"];
+function sentanceConstructor() {
+    const rand1 = Math.floor(Math.random() * (signs.length - 1));
+}
